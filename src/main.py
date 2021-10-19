51619d5cd92d32e921b1ffe47de5c6be91e02f84
carvilgar1 <carvilgar1@us.es>
2021-10-19 08:45:42 +0200

    VENTANA RAIZ CON MENU CREADA

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import sqlite3
 import urllib.request
 import re
+import os, ssl
 
 #Error SSL
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
@@ -37,6 +38,41 @@ def instanciar_db():
     arbol = BeautifulSoup(web, 'lxml')
     con.commit()
 
+def cargar_datos():
+    crear_tablas()
+    instanciar_db()
+
+def listar_libros():
+    None
+
+def listar_librerias():
+    None
+
+def libros_por_editorial():
+    None
+
+def libros_por_titulo_o_autor():
+    None
+
 if __name__ == '__main__':
-    raiz = Tk()
-    raiz.mainloop()
+    #Este codigo permite generar una ventana rai­z con un menu
+    root = Tk()
+    menu = Menu(root)
+    fila1 = Menu(menu, tearoff=0)
+    fila1.add_command(label='Cargar', command=cargar_datos)
+    fila1.add_command(label='Salir', command=root.quit)
+
+    opcion_datos = menu.add_cascade(label='Datos', menu = fila1)
+
+    fila2 = Menu(menu, tearoff=0)
+    fila2.add_command(label='Libros', command=listar_libros)
+    fila2.add_command(label="Librerias", command=listar_librerias)
+    opcion_buscar = menu.add_cascade(label='Listar', menu = fila2)
+
+    fila3 = Menu(menu, tearoff=0)
+    fila3.add_command(label='Libros por editorial', command=libros_por_editorial)
+    fila3.add_command(label="Libros por título o autor", command=libros_por_titulo_o_autor)
+    opcion_buscar = menu.add_cascade(label='Buscar', menu = fila3)
+
+    root.configure(menu=menu)
+    root.mainloop()
