fdd09953d40c709428f435efa4a0a4e992eb3c6d
carvilgar1 <carvilgar1@alum.us.es>
2021-11-09 08:46:09 +0100

    VENTANA RAIZ IMPLEMENTADA

--- practica-2/main.py
+++ practica-2/main.py
@@ -57,5 +57,62 @@ def ejemplo_busqueda():
         myquery = MultifieldParser(["asunto","contenido"], ix.schema).parse('query')
         results = searcher.search(myquery)
 
+def buscar_titulo():
+    '''muestre  una  ventana  con un entry que permita al  
+    usuario  introducir varias palabras, y muestre  en  otra ventana  (con una  
+    listbox    con    scrollbar)  todas  las  noticias  (categoría,  fecha,  título)  que 
+    contengan alguna de esas palabras en el título.'''
+    #TODO
+
+def buscar_resumen_titulo():
+    #TODO
+    '''muestre  una  ventana  con un entry que 
+    permita al  usuario  introducir una frase, y muestre  en  otra ventana  (con una  
+    listbox  con  scrollbar) todas las noticias (categoría, fecha, título, resumen) 
+    que contengan esa frase en el resumen o el título. '''
+
+def buscar_fecha():
+    #TODO
+    '''muestre  una  ventana  con un entry que  permita  al  
+    usuario  introducir una fecha (en formato dd MMM aaaa, por ejemplo ’11 
+    nov 2021’), y  muestre  en  otra ventana (con una  listbox  con  scrollbar)  
+    todas las noticias (categoría, fecha, título) desde ese día (inclusive)'''
+    
+
+def buscar_categoria():
+    #TODO
+    '''muestre   una    spinbox  que  permita  al    usuario  
+    seleccionar una categoría de todas las que hay en el índice, y muestre  en  otra 
+    ventana  (con una  listbox  con  scrollbar) todas las noticias (categoría, fecha, 
+    título) de dicha categoría.'''
+
+def eliminar_noticia():
+    #TODO
+    '''muestre una ventana con un botón y un entry que 
+    permita al usuario introducir un título de noticias (o palabras que estén en el título).  
+    Cuando se pulse el botón, se muestra una lista con las noticias (categoría, fecha, 
+    título) con dicho título. A continuación se muestra una ventana para que el usuario 
+    confirme que desea realizar los cambios. Si se confirma, se eliminan dichas noticias'''
+
 if __name__ == '__main__':
-    crea_index('indice')
+    #Este codigo permite generar una ventana raiz con un menu
+    root = Tk()
+    menu = Menu(root)
+    fila1 = Menu(menu, tearoff=0)
+    fila1.add_command(label='Cargar', command=lambda: crea_index('indice'))
+    fila1.add_command(label="Salir", command=root.destroy)
+
+    opcion_datos = menu.add_cascade(label='Datos', menu = fila1)
+
+    fila2 = Menu(menu, tearoff=0)
+    fila2.add_command(label='Título', command=buscar_titulo)
+    fila2.add_command(label="Resumen o Título", command=buscar_resumen_titulo)
+    fila2.add_command(label="Fecha", command=buscar_fecha)
+    fila2.add_command(label="Categoría", command=buscar_categoria)
+
+    opcion_buscar = menu.add_cascade(label='Buscar', menu = fila2)
+
+    menu.add_command(label='Eliminar Noticias', command=eliminar_noticia)
+
+    root.configure(menu=menu)
+    root.mainloop()
