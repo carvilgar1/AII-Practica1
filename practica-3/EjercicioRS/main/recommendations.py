b80123786afb8d3533a5c64b03f67e0c162c017a
pabgonmon2 <pablogonmon@gmail.com>
2021-12-21 10:29:53 +0100

    .

--- practica-3/EjercicioRS/main/recommendations.py
+++ practica-3/EjercicioRS/main/recommendations.py
@@ -11,19 +11,19 @@ def load_similarities():
     shelf['similarities'] = compute_similarities(book_attr, user_pref)
     shelf.close()
 
-# def recommended_books(user):
-#     shelf = shelve.open("dataRS.dat")
-#     #conjunto de artistas que ya ha escuchado el usuario, que no se consideran para recomendar
-#     listened = set()
-#     listened = set(a.artist.id for a in UserArtist.objects.filter(user=user))
-#     res = []
-#     for artist_id, score in shelf['similarities'][user]:
-#         if artist_id not in listened:
-#             artist_name = Artist.objects.get(pk=artist_id).name
-#             res.append([artist_name, 100 * score])
-#     shelf.close()
-#     print(res)
-#     return res
+def recommended_books(userId):
+    shelf = shelve.open("dataRS.dat")
+    #conjunto de artistas que ya ha escuchado el usuario, que no se consideran para recomendar
+    read = set()
+    read = set(a.libro for a in Rating.objects.filter(userId=userId))
+    res = []
+    for idLibro, score in shelf['similarities'][userId]:
+        if idLibro not in read:
+            book = Libro.objects.get(pk=idLibro).name
+            res.append([book, 100 * score])
+    shelf.close()
+    print(res)
+    return res
 
 def compute_similarities(book_attr, user_pref):
     print('Computing user-book similarity matrix')
@@ -31,7 +31,7 @@ def compute_similarities(book_attr, user_pref):
     for u in user_pref:
         top_books = {}
         for a in book_attr:
-            top_books[a] = dice_coefficient(user_pref[u], book_attr[a])
+            top_books[a] = dice_coefficient(set(user_pref), set(book_attr))
         res[u] = Counter(top_books).most_common(10)
     return res
 
@@ -40,12 +40,14 @@ def books_attr():
     books = {}
     
     for element in Libro.objects.all():
-        book_id = element.id
+        book_id = element.idLibro
         autor = element.autor
         genero = element.genero
         idioma = element.idioma
         books.setdefault(book_id, [])
-        books[book_id].append(autor,genero,idioma)
+        books[book_id].append(autor)
+        books[book_id].append(genero)
+        books[book_id].append(idioma)
     return books
 
 def user_preferences():
@@ -56,9 +58,9 @@ def user_preferences():
         users.setdefault(userId, {})
         libro = element.libro
         if(element.rating>=4):
-            users[userId][libro.id] = [libro.autor,libro.genero,libro.idioma]
+            users[userId][libro.idLibro] = [libro.autor,libro.genero,libro.idioma]
     for u in users:
-        users[u] = [[autor,genero,idioma] for libro,autor,genero,idioma in Counter(users[u]).most_common(5)]
+        users[u] = [[attr[0], attr[1],attr[2]] for libro,attr in Counter(users[u]).most_common(5)]
     return users
 
 def dice_coefficient(set1, set2):
